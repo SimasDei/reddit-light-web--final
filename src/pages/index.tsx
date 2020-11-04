2e63203901f6eb28a04052a8e72b8e1ac22c8dbb
Simas Deimantavicius <simonas.deimantavicius@teliacompany.com>
2020-11-04 10:50:28 +0200

    add final stage of web project

---
+++ src/pages/index.tsx
@@ -0,0 +1,80 @@
+import { Box, Button, Flex, Heading, Link, Stack, Text } from '@chakra-ui/core';
+import NextLink from 'next/link';
+import { EditDeletePostButtons } from '../components/EditDeletePostButtons';
+import { Layout } from '../components/Layout';
+import { UpdootSection } from '../components/UpdootSection';
+import { usePostsQuery, PostsQuery } from '../generated/graphql';
+import { withApollo } from '../utils/withApollo';
+
+const Index = () => {
+  const { data, error, loading, fetchMore, variables } = usePostsQuery({
+    variables: {
+      limit: 5,
+      cursor: null,
+    },
+    notifyOnNetworkStatusChange: true,
+  });
+
+  if (!loading && !data) {
+    return (
+      <div>
+        <div>you got query failed for some reason</div>
+        <div>{error?.message}</div>
+      </div>
+    );
+  }
+
+  return (
+    <Layout>
+      {!data && loading ? (
+        <div>loading...</div>
+      ) : (
+        <Stack spacing={8}>
+          {data!.posts.posts.map((p) =>
+            !p ? null : (
+              <Flex key={p.id} p={5} shadow="md" borderWidth="1px">
+                <UpdootSection post={p} />
+                <Box flex={1}>
+                  <NextLink href="/post/[id]" as={`/post/${p.id}`}>
+                    <Link>
+                      <Heading fontSize="xl">{p.title}</Heading>
+                    </Link>
+                  </NextLink>
+                  <Text>posted by {p.creator.username}</Text>
+                  <Flex align="center">
+                    <Text flex={1} mt={4}>
+                      {p.textSnippet}
+                    </Text>
+                    <Box ml="auto">
+                      <EditDeletePostButtons id={p.id} creatorId={p.creator.id} />
+                    </Box>
+                  </Flex>
+                </Box>
+              </Flex>
+            )
+          )}
+        </Stack>
+      )}
+      {data && data.posts.hasMore ? (
+        <Flex>
+          <Button
+            onClick={() => {
+              fetchMore({
+                variables: {
+                  limit: variables?.limit,
+                  cursor: data.posts.posts[data.posts.posts.length - 1].createdAt,
+                },
+              });
+            }}
+            isLoading={loading}
+            m="auto"
+            my={8}>
+            load more
+          </Button>
+        </Flex>
+      ) : null}
+    </Layout>
+  );
+};
+
+export default withApollo({ ssr: true })(Index);
